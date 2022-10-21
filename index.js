08247d43a389e0c446a6a6789346e40045cfa1f4
elibolonur <korax11@hotmail.com>
2022-10-21 22:58:00 +0200

    chore: remove unused

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Default config
-const js = require.resolve('./javascript')
-const vue = require.resolve('./vue')
 const vue = require.resolve('./vue3')
 
 module.exports = {
-  // Export by default JS & Vue config
   extends: [vue],
 }
