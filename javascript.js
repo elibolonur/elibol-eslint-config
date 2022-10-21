8512e1a9c9744139e193656e724c911bab79e4d7
elibolonur <korax11@hotmail.com>
2022-10-21 22:57:50 +0200

    chore: rename comment

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 /**
 |--------------------------------------------------
-| JavaScript rules, inherits eslint:recommended
+| JavaScript config
 |--------------------------------------------------
 **/
 
